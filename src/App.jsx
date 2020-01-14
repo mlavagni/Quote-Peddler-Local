49fe74aa1af461adaf9230a9b1594d087fad7e7e
Andrew Murray <darbyguitars@gmail.com>
2020-01-14 14:28:08 -0600

    build nav implement styles
    	modified:   package.json
    	modified:   src/App.jsx
    	modified:   src/components/Nav.js
    	new file:   src/styles/StyledNavWrapper.js
    	modified:   yarn.lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Button from './components/Button'
 import './App.css'
 import { store, cart } from './data'
 import Store from './views/Store'
+import Nav from './components/Nav'
 
 class App extends Component {
 	state = {
@@ -20,9 +22,17 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="App">
-				<Store store={this.state.storeData} />
-			</div>
+			<BrowserRouter>
+				<Switch>
+					<Nav>
+						<Route
+							exact
+							path="/"
+							render={() => <Store store={this.state.storeData} />}
+						/>
+					</Nav>
+				</Switch>
+			</BrowserRouter>
 		)
 	}
 }
