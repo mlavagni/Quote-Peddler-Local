2c4b8671c741598234d610b373d917ae4e8a26bb
mark <mark@marks-MacBook-Pro.local>
2020-01-15 09:31:47 -0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Button from './components/Button'
-import './App.css'
-import { store, cart } from './data'
-import Store from './views/Store'
-import Nav from './components/Nav'
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Button from "./components/Button";
+import "./App.css";
+import { store, cart } from "./data";
+import Store from "./views/Store";
+import Nav from "./components/Nav";
+// test
 class App extends Component {
-	state = {
-		storeData: [],
-		cartData: []
-	}
+  state = {
+    storeData: [],
+    cartData: []
+  };
 
-	componentDidMount() {
-		this.setState({
-			storeData: store,
-			cartData: cart
-		})
-	}
+  componentDidMount() {
+    this.setState({
+      storeData: store,
+      cartData: cart
+    });
+  }
 
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Nav>
-						<Route
-							exact
-							path="/"
-							render={() => <Store store={this.state.storeData} />}
-						/>
-					</Nav>
-				</Switch>
-			</BrowserRouter>
-		)
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Nav>
+            <Route
+              exact
+              path="/"
+              render={() => <Store store={this.state.storeData} />}
+            />
+          </Nav>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
